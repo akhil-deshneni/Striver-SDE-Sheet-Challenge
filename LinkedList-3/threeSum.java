d6614967efaf99c48709c41858ba46340e7a0247
unknown <Akhil.Deshneni01@student.csulb.edu>
2023-08-01 19:50:00 -0700

    linkedlist-day-3

--- LinkedList-3/threeSum.java
+++ LinkedList-3/threeSum.java
@@ -1,6 +1,36 @@
+import java.util.*;
 public class threeSum {
+
+    public void approachOne(int[] nums,List<List<Integer>> ans){
+        int n = nums.length;
+        List<Integer> temp = new ArrayList<>();
+        for(int i=0;i<n-3;i++){
+            for(int j=i+1;j<n;j++) {
+                for(int k=j+1;k<n;k++){
+                    if(nums[i] + nums[j] + nums[k] == 0) {
+                        
+                        temp.add(nums[i]);temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        ans.add(temp);
+                        temp = new ArrayList<>();
+                    }
+                }
+            }
+            while(i < n && nums[i] == nums[i+1]) i+=1;
+        }
+    }
+
+    public List<List<Integer>> triplets(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();;
+        approachOne(nums,ans);
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {-1,0,1,2,-1,-4};
-        
+        int[] arr = {0,0,0,0,0,0,0};
+        threeSum ts = new threeSum();
+        List<List<Integer>> ans = ts.triplets(arr);
+        System.out.println(ans);
     }
 }
